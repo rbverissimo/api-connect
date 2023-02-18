e382097b70f07d58a9b4eeb9bb47f41522a677ba
rbverissimo <rverissimo.dev@gmail.com>
2023-02-17 23:22:15 -0300

    Working logic on how to fetch all the usersnames from the users in the API implemented.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var request = new XMLHttpRequest();
 request.open("GET", "https://jsonplaceholder.typicode.com/users", true);
 request.onload = function () {
-  // logic of how to access the API
+  var data = JSON.parse(this.response);
+
+  data.forEach((users) => {
+    console.log(users.username);
+  });
 };
 
 //Sending the request

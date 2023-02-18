33ca116db9e2d935d7d10718bfd642ce5ade8e51
rbverissimo <rverissimo.dev@gmail.com>
2023-02-17 23:27:28 -0300

    Added some comments, nothing else

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+//Create the object that will handle the request
 var request = new XMLHttpRequest();
+
+//Open connection
 request.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+
+//What happens once the connection is established
 request.onload = function () {
   var data = JSON.parse(this.response);
 
-  data.forEach((users) => {
-    console.log(users.username);
-  });
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach((users) => {
+      console.log(users.username);
+    });
+  } else {
+    console.log("error");
+  }
 };
 
 //Sending the request
